1a0ca6174bfea38f0f2bbf131aa4d34193caf8a3
swornim-csit <karmaghising740@gmail.com>
2022-11-29 16:37:34 +0545

    Change background color for sidebar

--- context/StateContext.jsx
+++ context/StateContext.jsx
@@ -8,8 +8,8 @@ const StateContext = ({children}) => {
 
   const darkTheme = () =>{
     if(darkMode){
-      const theme0 = {background: "#0c0600"}
-      const theme1 = {background: "#1f221f", color: "#fff"}
+      const theme0 = {background: "#0f1624"}
+      const theme1 = {background: "#111827", color: "#fff"}
       const imgNeg = {filter: "invert(100%)"}
       const theme2 = {border: ".5px solid #fff", color: "#fff"}
       const boxShadow = {boxShadow: "5px 5px 20px #000000"}
